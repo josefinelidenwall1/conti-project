cbf8c29c068658f9cff189ed26dbd84246aab839
Jualure <juaquin.rehnberg@appliedtechnology.se>
2026-01-22 22:59:38 +0100

    added debugg dir location for creation of file

--- manualtrigger.py
+++ manualtrigger.py
@@ -1,10 +1,15 @@
 #manual trigger
+import os
 from report_queries import run_report
 from sdkfunctions import sdk_config, upload_file2
 
 
 def get_report():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
+    print(f"DEBUGG: workign directory set to: {script_dir}")
     run_report()
+    
     sdk_config()
     upload_file2('consultant_report.txt')
 
